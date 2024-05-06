08b3daf61c431cf4343435d0b305e0f3ea393859
Andres Azcona <andresazconagla@gmail.com>
2024-05-06 09:59:58 -0500

    vercel

--- src/app.js
+++ src/app.js
@@ -19,6 +19,13 @@ app.use('/api/form', authMiddleware.verifyToken, formController);
 app.use('/api/contacts', authMiddleware.verifyToken, contactsController);
 
 const PORT = process.env.PORT || 3000;
+
+// Manejador de ruta para la raíz "/"
+app.get("/", (req, res) => {
+  res.send(`<h2>El servidor está funcionando en el puerto ${PORT}</h2>`);
+});
+
+// Manejador de ruta para cualquier otra ruta
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 });
