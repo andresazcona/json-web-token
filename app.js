4861a0be28b2cfc1d95e0315d2c16b811d93a3bd
Andres Azcona <andresazconagla@gmail.com>
2024-05-11 17:29:57 -0500

    triple mlp vercel

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const profileController = require('./src/controllers/profileController');
 const formController = require('./src/controllers/formController');
 const contactsController = require('./src/controllers/contactsController');
 
+
 const app = express();
 
 app.use(express.json());
@@ -18,12 +19,10 @@ app.use('/api/profile', authMiddleware.verifyToken, profileController);
 app.use('/api/form', authMiddleware.verifyToken, formController);
 app.use('/api/contacts', authMiddleware.verifyToken, contactsController);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Manejador de ruta para la raíz "/"
-app.get("/", (req, res) => {
-  res.send(`<h2>El servidor está funcionando en el puerto ${PORT}</h2>`);
-});
+app.get("/", (req, res) => res.send("servidor en linea"));
 
 // Manejador de ruta para cualquier otra ruta
 app.listen(PORT, () => {
